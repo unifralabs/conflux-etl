634798c6eb9c9806ebeaf085c97c524e2e804e2a
paul-unifra <paul@unifra.io>
2022-07-07 11:56:01 +0800

    fix(export_all): fix web3 provider and contract_address field

--- ethereumetl/jobs/export_all_common.py
+++ ethereumetl/jobs/export_all_common.py
@@ -27,18 +27,23 @@ import os
 import shutil
 from time import time
 
-from ethereumetl.csv_utils import set_max_field_size_limit
 from blockchainetl.file_utils import smart_open
+from ethereumetl.csv_utils import set_max_field_size_limit
 from ethereumetl.jobs.export_blocks_job import ExportBlocksJob
 from ethereumetl.jobs.export_contracts_job import ExportContractsJob
 from ethereumetl.jobs.export_receipts_job import ExportReceiptsJob
 from ethereumetl.jobs.export_token_transfers_job import ExportTokenTransfersJob
 from ethereumetl.jobs.export_tokens_job import ExportTokensJob
-from ethereumetl.jobs.exporters.blocks_and_transactions_item_exporter import blocks_and_transactions_item_exporter
-from ethereumetl.jobs.exporters.contracts_item_exporter import contracts_item_exporter
-from ethereumetl.jobs.exporters.receipts_and_logs_item_exporter import receipts_and_logs_item_exporter
-from ethereumetl.jobs.exporters.token_transfers_item_exporter import token_transfers_item_exporter
-from ethereumetl.jobs.exporters.tokens_item_exporter import tokens_item_exporter
+from ethereumetl.jobs.exporters.blocks_and_transactions_item_exporter import \
+    blocks_and_transactions_item_exporter
+from ethereumetl.jobs.exporters.contracts_item_exporter import \
+    contracts_item_exporter
+from ethereumetl.jobs.exporters.receipts_and_logs_item_exporter import \
+    receipts_and_logs_item_exporter
+from ethereumetl.jobs.exporters.token_transfers_item_exporter import \
+    token_transfers_item_exporter
+from ethereumetl.jobs.exporters.tokens_item_exporter import \
+    tokens_item_exporter
 from ethereumetl.providers.auto import get_provider_from_uri
 from ethereumetl.thread_local_proxy import ThreadLocalProxy
 from ethereumetl.web3_utils import build_web3
@@ -146,7 +151,7 @@ def export_all_common(partitions, output_dir, provider_uri, max_workers, batch_s
                 start_block=batch_start_block,
                 end_block=batch_end_block,
                 batch_size=batch_size,
-                web3=ThreadLocalProxy(lambda: build_web3(get_provider_from_uri(provider_uri))),
+                web3=ThreadLocalProxy(lambda: get_provider_from_uri(provider_uri)),
                 item_exporter=token_transfers_item_exporter(token_transfers_file),
                 max_workers=max_workers)
             job.run()
@@ -211,10 +216,10 @@ def export_all_common(partitions, output_dir, provider_uri, max_workers, batch_s
             cache_output_dir=cache_output_dir,
             file_name_suffix=file_name_suffix,
         )
-        logger.info('Extracting contract_address from receipt file {receipts_file}'.format(
+        logger.info('Extracting contract_created from receipt file {receipts_file}'.format(
             receipts_file=receipts_file
         ))
-        extract_csv_column_unique(receipts_file, contract_addresses_file, 'contract_address')
+        extract_csv_column_unique(receipts_file, contract_addresses_file, 'contract_created')
 
         contracts_output_dir = '{output_dir}/contracts{partition_dir}'.format(
             output_dir=output_dir,
